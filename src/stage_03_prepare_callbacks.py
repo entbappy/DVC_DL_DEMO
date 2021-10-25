65cffd84bd2272211a88b2922df2cbc1eb6ed0c9
Bappy Ahmed <entbappy73@gmail.com>
2021-10-25 23:43:21 +0600

    updated

--- src/stage_03_prepare_callbacks.py
+++ src/stage_03_prepare_callbacks.py
@@ -10,8 +10,9 @@ os.makedirs(log_dir, exist_ok=True)
 logging.basicConfig(filename=os.path.join(log_dir, 'running_logs.log'), level=logging.INFO, format=logging_str,
                     filemode="a")
 
-def prepare_callbacks(config_path):
+def prepare_callbacks(config_path, params_path):
     config = read_yaml(config_path)
+    params = read_yaml(params_path)
 
     artifacts = config["artifacts"]
     artifacts_dir = artifacts["ARTIFACTS_DIR"]
@@ -37,12 +38,13 @@ if __name__ == '__main__':
     args = argparse.ArgumentParser()
 
     args.add_argument("--config", "-c", default="config/config.yaml")
+    args.add_argument("--params", "-p", default="params.yaml")
 
     parsed_args = args.parse_args()
 
     try:
         logging.info(">>>>> stage three started")
-        prepare_callbacks(config_path=parsed_args.config)
+        prepare_callbacks(config_path=parsed_args.config, params_path=parsed_args.params)
         logging.info("stage three completed! callbacks are prepared and saved as binary>>>>>")
     except Exception as e:
         logging.exception(e)
